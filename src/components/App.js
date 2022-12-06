9b5326563f3dc02a455474a7bd591395302b1c87
Leah Cardoz <leahcardoz@gmail.com>
2022-12-06 13:11:21 -0500

    fixed switch react router dom issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import {Switch, Route, useHistory} from 'react-router-dom';
 
 import NavBar from './NavBar';
 import Home from './Home';
@@ -7,25 +7,23 @@ import Artists from './Artists';
 import Prints from './Prints';
 
 function App() {
-
-
-  return (
-    <div className="App">
-      <h1>App component</h1>
-      <NavBar />
-      <Switch>
-        <Route path="/artists">
-          <Artists />
-        </Route>
-        <Route path="/prints">
-          <Prints />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>App component</h1>
+			<NavBar />
+			<Switch>
+				<Route path='/artists'>
+					<Artists />
+				</Route>
+				<Route path='/prints'>
+					<Prints />
+				</Route>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
