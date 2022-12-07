bfcd41108ef0f3c44fa20ff79e473b105f8943ac
Aaron David <ajdavid128@gmail.com>
2022-12-07 16:29:54 -0600

    built out mouse over/mouse out event on PrintCard component

--- src/components/PrintCard.js
+++ src/components/PrintCard.js
@@ -1,17 +1,39 @@
+import {useState} from 'react'
+
 
 function PrintCard ({title, year, edition_size, category, sub_category, image_url}) {
 
-    const categoryToggle = sub_category === ""? null : `, ${sub_category}`
+    const [posterInfo, setPosterInfo] = useState(false);
+
+    const handleMouseOver = () => {
+        setPosterInfo(true);
+      }
+      
+    const handleMouseOut = () => {
+        setPosterInfo(false);
+      }
+
+    const FlippedCard = () => {
+        return (
+          <div>
+            <h3><u>Title:</u> <br/>{title}</h3>
+            <h3><u>Year:</u> <br/>{year}</h3>
+            <h3><u>Artist:</u> <br/>{}</h3>
+          </div>
+        );
+    }
+    
+    const cardSide = posterInfo ? <FlippedCard /> : <img id="poster-image" src={image_url} alt="screen printed poster"/>
+
+    // const categoryToggle = sub_category === ""? null : `, ${sub_category}`
 
     return (
-        <div id="printCard">
-            <img id="poster-image" src={image_url} alt="screen printed poster" />
-            {/* <div>
-                <h2>Title: "{title}"</h2>
-                <p>Year: {year}</p>
-                <p>Edition Size: {edition_size}</p>
-                <p>Art Category: {category} {categoryToggle}</p>
-            </div> */}
+        <div 
+            id="printCard"
+            onMouseOver={handleMouseOver} 
+            onMouseOut={handleMouseOut} 
+            >
+            {cardSide}
         </div>
     )
 }
